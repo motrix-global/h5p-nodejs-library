037dd89117c5cfecfd89a293ba8322aad75d6354
Reinaldo Mello <rmello@gmail.com>
2024-06-14 09:42:56 -0300

    fix(server): update core version

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,12 @@
 # If the editor and core files are missing, we download them from GitHub.
 if [ ! -d "packages/h5p-examples/h5p/editor" ] || [ ! -d "packages/h5p-examples/h5p/core" ]
-then    
-    sh packages/h5p-examples/download-core.sh 661d4f6c7d7b1117587654941f5fcf91acb5f4eb 0365b081efa8b55ab9fd58594aa599f9630268f6
+then
+    sh packages/h5p-examples/download-core.sh 1.26 1.25
 else
     echo "Not downloading H5P Core and Editor files as they are already present!"
 fi
 
-# We only download the content examples if the script is not executed in a 
+# We only download the content examples if the script is not executed in a
 # CI environment. In a CI environment the scripts must be downloaded later to
 # be able to squeeze in the cache restore before downloading them.
 if [ "$CI" != "true" ]
